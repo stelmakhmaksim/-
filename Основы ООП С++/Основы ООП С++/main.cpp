3f490d4e38fcc51cf9aaecac65ae8bae095db7a3
stelmakhmaksim <stelmakh96@mail.ru>
2016-08-03 16:31:03 +0300

    Add struct

--- Основы ООП С++/Основы ООП С++/main.cpp
+++ Основы ООП С++/Основы ООП С++/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 using namespace std;
 
+struct Book//в структуре не происходит выделение памяти, в отличии от функции main
+{
+	int id;//поле структуры
+	int pages;
+	float cost;
+};
+
+void write(Book b)
+{
+	cout << "Идентификатор книги: " << b.id << endl;
+	cout << "Кол-во страниц книги: " << b.pages << endl;
+	cout << "Стоимость книги: " << b.cost << endl << endl;
+}
+
+
 int main()
 {
-	cout << "Hello world!!!\n";
+	setlocale(0, "");
+
+	Book b1;
+	b1.id = 1;
+	b1.pages = 150;
+	b1.cost = 99.9;
+	write(b1);
+
+	b1 = { 1, 15, 33 };
+	write(b1);
+
+	Book b2 = { 2, 300, 130.5 };
+	write(b2);
+
+	Book b3;
+	b3 = { 3, 5, 8 };
+	write(b3);
+
+	b3 = b1;
+	write(b3);
+
 	system("pause");
+	return 0;
 }
