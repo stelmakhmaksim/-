fd434bec6ee1c8a97d427c97045281148d2470a5
stelmakhmaksim <stelmakh96@mail.ru>
2016-08-18 23:04:25 +0300

    перегрузка cin и cout, как записывать и читать пользовательские данные из файла

--- Основы ООП С++/Основы ООП С++/main.cpp
+++ Основы ООП С++/Основы ООП С++/main.cpp
@@ -1,61 +1,33 @@
-#include "main.h"
-	//форматированный ввод и вывод данных целесообразно использовать только при небольшой величине и малом количестве
-	//а при большом количестве целесообразно использовать двоичный ввод/вывод, также, как данные храняться в оперативной памяти
-//посимвольный ввод/вывод
-void write_file(string str1)
-{
-	string str = str1;
-	ofstream output;
-	output.open("file.txt");
-	for (int i = 0; i < str.size(); i++)
-		output.put(str[i]);
-}
+#include "something.h"
 
-void read_file()
+//как записывать в файл и читать оттуда пользовательские типы данных
+void write_read()
 {
-	ifstream input;
-	input.open("file.txt");
-	
-	cout<<input.rdbuf()<<endl;//функция просто берет всё и возвращает
-	/*char ch;
-	while (input.get(ch))//перебираем все символы, пока не дойдем до конца файла
-		cout << ch;
-	cout << endl;*/
-}
+	Something some("string", 7);
+	fstream file("file.dat", ios::binary | ios::out | ios::in);//позволяет создать объект для записи и чтения информации в файл
+	file.write(reinterpret_cast<char*>(&some), sizeof(Something));
 
-//двоичный ввод/вывод
-void write_file1(int* arr1)
-{
-	int arr[10];
-	for (int i = 0; i < 10; i++)
-		arr[i] = *(arr1 + i);
-	ofstream output("file.txt", ios::binary);
-	output.write(reinterpret_cast<char*>(arr), sizeof(arr));
-	output.close();
-}
+	cout << file.tellg() << endl;//для того, чтобы получить информацию, где у нас сейчас стоит указатель
+	file.seekg(0);//указатель стоит в конце, т.к. мы уже что-то записали в файл, а нам нужно перенести в начало
+	cout << file.tellg() << endl;
 
-void read_file1(int* arr)
-{
-	int arr2[10];
-	ifstream input("file.txt", ios::binary);
-	input.read(reinterpret_cast<char*>(arr2), sizeof(arr2));
-	input.close();
-
-	for (int i = 0; i < sizeof(arr2)/sizeof(int); i++)
-		cout << arr[i] << " - " << arr2[i] << endl;
+	Something some2;
+	file.read(reinterpret_cast<char*>(&some2), sizeof(some2));//для совместимости зачитанного и прочитанного объекта, достаточно, чтобы они
+	some2.showSomething();//имели одинаковый набор полей, даже, если у них будут разные методы, данные всё равно отобразятся корректно
+	//утверждение справедливо для классов, которые не содержат виртуальные функции
 }
 int main()
 {
 	setlocale(0, "");
-	/*//put() для записи символа в файл и get() для получения символа из файла
-	string str1 = "Just some text!";
-	write_file(str1);
-	read_file();*/
 
-	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	write_file1(arr);
-	read_file1(arr);
+	//write_read();
 
+	//перегрузка операторов ввода и вывода
+	Something some1, some2;
+	cout << some1 << some2;
+	cin >> some1 >> some2;
+	//как сделать не для cin и cout написать самим
+	//передача по ссылке и указателю
 	system("pause");
 	return 0;
 }
