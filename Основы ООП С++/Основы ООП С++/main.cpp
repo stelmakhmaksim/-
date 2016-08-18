6cb4936c5a1359ea0a115e171c82e09d9b9a5613
stelmakhmaksim <stelmakh96@mail.ru>
2016-08-18 12:05:58 +0300

    запись и чтение файла

--- Основы ООП С++/Основы ООП С++/main.cpp
+++ Основы ООП С++/Основы ООП С++/main.cpp
@@ -1,32 +1,83 @@
 #include "main.h"
-//флаги статуса ошибок
-//goodbit-ошибок нет||eofbit-достигнут конец файла||failbit-операция не выполнена||badbit-недопустимая операция||hardfail-неисправимая ошибка
-//good(),eof(),fail(),bad()-позволяют определетить статус флагов||clear-снимает все флаги ошибок или устанавливает соответствующий флаг
+//форматироавнный ввод/вывод: число 6.02 вместо того, чтобы храниться в float, хранится в виде последовательности символов '6' ',' '0' '2'
+
+void create_file();
+void read_file();
+
+void create_file1();
+void read_file1();
 
 int main()
 {
 	setlocale(0, "");
-	cin.unsetf(ios::skipws);
-	int i;
-	while (true)
-	{
-  		cout << "Введите целое число: ";
-		cin >> i;//если вводить 1453asds, то компилятор съест только цифры, а буквы остануться в потоке
 
-		if (cin.good())
-		{
-			cin.ignore(10, '\n');//чистим поток
-			break;
-		}
+	//create_file();
+	//read_file();
 
-		cin.clear();//снимаем флаги ошибок//если мы допустили ошибку один раз и не почистили её,то она будет стоять постоянно,(зацикливание)
-		cout << "Неправильный ввод" << endl;
-		cin.ignore(10, '\n');//если поток не чистить, то данные из потока будут постоянно пытаться присовиться i, результат - зацикливание
-	}
-	cout << "Целое число " << i << endl;
-	string str;
-	cin >> str;//смотрим, что осталось в потоке
-	cout << "То, что осталось в потоке: " << str << endl;
+	create_file1();
+	read_file1();
+
 	system("pause");
 	return 0;
+}
+
+void create_file()
+{
+	char ch = 'x';
+	int i = 7;
+	double d = 3.21;
+	string str1 = "String1", str2 = "String 2";
+
+	ofstream outfile("file.txt");//если нет такого файла, то он автоматически создается, а если есть, то перезаписывается
+	if (!outfile.is_open())
+		cout << "Не удается создать файл для записи" << endl;
+	else cout << "Файл успешно создан." << endl;
+	outfile << ch << ' ' << i << ' ' << d << ' ' << str1 << ' ' << str2 << endl;
+}
+
+void read_file()//этот подход не позволяет обрабатывать строки содержащие пробелы
+{
+	char ch;
+	int i;
+	double d;
+	string str1, str2;
+
+	ifstream infile("file.txt");
+	if (!infile.is_open())
+		cout << "Не удается открыть файл для чтения." << endl;
+	else
+	{
+		cout << "Файл открыт для чтения." << endl;
+		infile >> ch >> i >> d >> str1 >> str2;
+		cout << "ch= " << ch << "\ni= " << i << "\nd= " << d << "\nstr1= " << str1 << "\nstr2= " << str2 << endl;
+	}
+}
+
+void create_file1()
+{
+	ofstream outfile("file.txt");//если нет такого файла, то он автоматически создается, а если есть, то перезаписывается
+	if (!outfile.is_open())
+		cout << "Не удается создать файл для записи" << endl;
+	else
+	{
+		cout << "Файл успешно создан." << endl;
+		outfile << "String 1\n" << "String 2\n" << "String 3\n" << "String 4";
+	}
+}
+
+void read_file1()//этот подход не позволяет обрабатывать строки содержащие пробелы
+{
+	char buffer[50];
+	ifstream infile("file.txt");
+	if (!infile.is_open())
+		cout << "Не удается открыть файл для чтения." << endl;
+	else
+	{
+		cout << "Файл открыт для чтения." << endl;
+		while (!infile.eof())//возвращает true, когда достигнут конец файла
+		{
+			infile.getline(buffer, 50);//если в строке больше 50 символов, то мы не считаем оставшиеся данные
+			cout << buffer << endl;
+		}
+	}
 }
