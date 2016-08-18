50c195a80105585c8f78844ac15af6426b0bf3a7
stelmakhmaksim <stelmakh96@mail.ru>
2016-08-18 18:22:56 +0300

    посимвольный и двоичный ввод/вывод

--- Основы ООП С++/Основы ООП С++/main.cpp
+++ Основы ООП С++/Основы ООП С++/main.cpp
@@ -1,83 +1,61 @@
 #include "main.h"
-//форматироавнный ввод/вывод: число 6.02 вместо того, чтобы храниться в float, хранится в виде последовательности символов '6' ',' '0' '2'
+	//форматированный ввод и вывод данных целесообразно использовать только при небольшой величине и малом количестве
+	//а при большом количестве целесообразно использовать двоичный ввод/вывод, также, как данные храняться в оперативной памяти
+//посимвольный ввод/вывод
+void write_file(string str1)
+{
+	string str = str1;
+	ofstream output;
+	output.open("file.txt");
+	for (int i = 0; i < str.size(); i++)
+		output.put(str[i]);
+}
 
-void create_file();
-void read_file();
+void read_file()
+{
+	ifstream input;
+	input.open("file.txt");
+	
+	cout<<input.rdbuf()<<endl;//функция просто берет всё и возвращает
+	/*char ch;
+	while (input.get(ch))//перебираем все символы, пока не дойдем до конца файла
+		cout << ch;
+	cout << endl;*/
+}
 
-void create_file1();
-void read_file1();
+//двоичный ввод/вывод
+void write_file1(int* arr1)
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+		arr[i] = *(arr1 + i);
+	ofstream output("file.txt", ios::binary);
+	output.write(reinterpret_cast<char*>(arr), sizeof(arr));
+	output.close();
+}
 
+void read_file1(int* arr)
+{
+	int arr2[10];
+	ifstream input("file.txt", ios::binary);
+	input.read(reinterpret_cast<char*>(arr2), sizeof(arr2));
+	input.close();
+
+	for (int i = 0; i < sizeof(arr2)/sizeof(int); i++)
+		cout << arr[i] << " - " << arr2[i] << endl;
+}
 int main()
 {
 	setlocale(0, "");
+	/*//put() для записи символа в файл и get() для получения символа из файла
+	string str1 = "Just some text!";
+	write_file(str1);
+	read_file();*/
 
-	//create_file();
-	//read_file();
-
-	create_file1();
-	read_file1();
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	write_file1(arr);
+	read_file1(arr);
 
 	system("pause");
 	return 0;
-}
-
-void create_file()
-{
-	char ch = 'x';
-	int i = 7;
-	double d = 3.21;
-	string str1 = "String1", str2 = "String 2";
-
-	ofstream outfile("file.txt");//если нет такого файла, то он автоматически создается, а если есть, то перезаписывается
-	if (!outfile.is_open())
-		cout << "Не удается создать файл для записи" << endl;
-	else cout << "Файл успешно создан." << endl;
-	outfile << ch << ' ' << i << ' ' << d << ' ' << str1 << ' ' << str2 << endl;
-}
-
-void read_file()//этот подход не позволяет обрабатывать строки содержащие пробелы
-{
-	char ch;
-	int i;
-	double d;
-	string str1, str2;
-
-	ifstream infile("file.txt");
-	if (!infile.is_open())
-		cout << "Не удается открыть файл для чтения." << endl;
-	else
-	{
-		cout << "Файл открыт для чтения." << endl;
-		infile >> ch >> i >> d >> str1 >> str2;
-		cout << "ch= " << ch << "\ni= " << i << "\nd= " << d << "\nstr1= " << str1 << "\nstr2= " << str2 << endl;
-	}
-}
-
-void create_file1()
-{
-	ofstream outfile("file.txt");//если нет такого файла, то он автоматически создается, а если есть, то перезаписывается
-	if (!outfile.is_open())
-		cout << "Не удается создать файл для записи" << endl;
-	else
-	{
-		cout << "Файл успешно создан." << endl;
-		outfile << "String 1\n" << "String 2\n" << "String 3\n" << "String 4";
-	}
-}
-
-void read_file1()//этот подход не позволяет обрабатывать строки содержащие пробелы
-{
-	char buffer[50];
-	ifstream infile("file.txt");
-	if (!infile.is_open())
-		cout << "Не удается открыть файл для чтения." << endl;
-	else
-	{
-		cout << "Файл открыт для чтения." << endl;
-		while (!infile.eof())//возвращает true, когда достигнут конец файла
-		{
-			infile.getline(buffer, 50);//если в строке больше 50 символов, то мы не считаем оставшиеся данные
-			cout << buffer << endl;
-		}
-	}
 }
